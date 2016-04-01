1b2415cd719c4e3167bfa7bd00d1eda2073e5854
Brooks <brooksryan19@gmail.com>
2016-04-01 15:07:37 -0700

    fixed the services bug

---
+++ services/visitorCounterService.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+function counter (){
+
+  this.count = 0;
+
+}
+
+counter.prototype.incrementCounterUp = function () {
+    this.count++;
+};
+
+counter.prototype.logCounter = function () {
+    console.log(this.count);
+};
+
+module.exports = {
+  counter : new counter()
+};
