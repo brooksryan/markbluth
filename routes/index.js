753494b9a8588a30026dcb3ec092eb7a92630928
Brooks <brooksryan19@gmail.com>
2016-04-01 15:05:49 -0700

    added a basic counter feature, need to eventually harden the function so that it actually references a database number.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var counter = require('../services/visitorCounterService');
+var thisCounter = counter.counter;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mark is our Friend' });
+  thisCounter.incrementCounterUp();
+  thisCounter.logCounter();
+  res.currentCount = thisCounter.count
+  next();
+},
+function(req, res, next){
+  res.render('index', {
+    title: 'Mark is our friend',
+    count: res.currentCount
+ });
 });
 
 module.exports = router;
