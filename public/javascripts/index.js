ac549e886ddbfd4bca13c0ac3e0b6da50ff994f0
Brooks <brooksryan19@gmail.com>
2016-05-10 12:38:55 -0700

    changed error message page, added a flexbox container to the main page to center and added a max width at 877px ish

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,59 +8,59 @@ var imageLinkRef = new Firebase("https://markbluth.firebaseio.com/images");
 //change the value of the "count" element in the HTML
 //keeping this number as the most updated and live changes
 ref.on("value", function(snapshot){
-  var data = snapshot.val();
-  var thisElement = document.getElementById("count");
-  thisElement.innerHTML = data;
-  console.log(data);
+    var data = snapshot.val();
+    var thisElement = document.getElementById("count");
+    thisElement.innerHTML = data;
+    console.log(data);
 });
 
 //file upload function
 function upload (file) {
 
-  //instantiates new form data to hand the request
-  var FD = new FormData();
+    //instantiates new form data to hand the request
+    var FD = new FormData();
 
-  //appends the binary image data to the request
-  //SIDENOTE: UNSURE IF THIS IS NECESSARY
-  FD.append('image', file);
+    //appends the binary image data to the request
+    //SIDENOTE: UNSURE IF THIS IS NECESSARY
+    FD.append('image', file);
 
-  //instantiate the new XMLHttpRequest
-  var httpRequest = new XMLHttpRequest();
+    //instantiate the new XMLHttpRequest
+    var httpRequest = new XMLHttpRequest();
 
-  //logs the state change when returned by
-  //ajax request
-  httpRequest.onreadystatechange = function(){
-    console.log(httpRequest.readyState);
-  };
+    //logs the state change when returned by
+    //ajax request
+    httpRequest.onreadystatechange = function(){
+        console.log(httpRequest.readyState);
+    };
 
-  //catches errors in request
-  httpRequest.addEventListener("error", function(event) {
-    alert('Oops! Something goes wrong.');
-    console.log(event);
-  });
+    //catches errors in request
+    httpRequest.addEventListener("error", function(event) {
+        alert('Oops! Something goes wrong.');
+        console.log(event);
+    });
 
-  //Opens AJAX to imjur upload endpoint on API
-  httpRequest.open('POST', 'https://api.imgur.com/3/image', true);
+    //Opens AJAX to imjur upload endpoint on API
+    httpRequest.open('POST', 'https://api.imgur.com/3/image', true);
 
-  //Sets authorization headers
-  httpRequest.setRequestHeader('Authorization','Client-ID a03e22577c6d470');
+    //Sets authorization headers
+    httpRequest.setRequestHeader('Authorization','Client-ID a03e22577c6d470');
 
-  //when the request has finished logs the link to
-  //the image uploaded.
-  //Function also saves the img url to firebase once
-  //it receives it
-  httpRequest.onload = function() {
-    var link = JSON.parse(httpRequest.responseText).data.link;
-    console.log(link);
+    //when the request has finished logs the link to
+    //the image uploaded.
+    //Function also saves the img url to firebase once
+    //it receives it
+    httpRequest.onload = function() {
+        var link = JSON.parse(httpRequest.responseText).data.link;
+        console.log(link);
 
-    //saves the image url to firebase
-    saveImageUrlToFirebase (link);
+        //saves the image url to firebase
+        saveImageUrlToFirebase (link);
 
-    //sets the image in the webpage to the image just
-    //uploaded
-    replaceCurrentImage(link);
+        //sets the image in the webpage to the image just
+        //uploaded
+        replaceCurrentImage(link);
 
-  };
+    };
 
   //sends request with FD output as body
   httpRequest.send(FD);
@@ -71,24 +71,22 @@ function upload (file) {
 //to the firebase reference /images and applies
 //a timestamp for ordering
 function saveImageUrlToFirebase (imgUrl) {
-  imageLinkRef.push({
-    imgUrl: imgUrl,
-    timestamp: Firebase.ServerValue.TIMESTAMP,
-  });
+    imageLinkRef.push({
+        imgUrl: imgUrl,
+        timestamp: Firebase.ServerValue.TIMESTAMP,
+    });
 }
 
 //retreives the image at pictureOfMark with the most
 //recent picture of mark received by the firebase databse
 imageLinkRef.orderByChild("timestamp").limitToLast(1).on("child_added", function(snapshot) {
-  var newImageUrl = snapshot.val().imgUrl;
-
-  replaceCurrentImage(newImageUrl);         //uses current image to replace
+    var newImageUrl = snapshot.val().imgUrl;
+    replaceCurrentImage(newImageUrl);         //uses current image to replace
                                             //image ofr mark in dom
 });
-// }
 
 //gets the picture of mark image tag and
 //replaces it with the provided url
 function replaceCurrentImage (imgUrl) {
-  document.getElementById("pictureOfMark").src = imgUrl;
+    document.getElementById("pictureOfMark").src = imgUrl;
 }
