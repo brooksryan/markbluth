e0cade583013e7c2427b1ba739297c03520077a3
Brooks <brooksryan19@gmail.com>
2017-01-06 18:57:07 -0800

    added styling to gallery

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -3,14 +3,17 @@ var imageLinkRef = new Firebase("https://markbluth.firebaseio.com/images");
 imageLinkRef.orderByChild("timestamp").limitToLast(100).on("child_added", function(snapshot) {
     var newImageUrl = snapshot.val().imgUrl;
 
+    var createDiv = document.createElement("div");
+
     var createImage = document.createElement("img");
 
     createImage.src = newImageUrl;
 
     var masterElement = document.getElementById("galleryDiv")
 
-    masterElement.appendChild(createImage);
+    masterElement.appendChild(createDiv);
 
-    console.log(newImageUrl);         //uses current image to replace
-                                            //image ofr mark in dom
+    createDiv.appendChild(createImage);
+
+    console.log(newImageUrl);
 });
